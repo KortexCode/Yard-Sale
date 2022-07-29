38fbd57f02ec3c6cf7dfceca34df2a39573b35a3
Maeno-codes <cacortesl01@gmail.com>
2022-07-29 14:27:29 -0500

    Ajustes a los estilos del navDesktopStyle, se integra mobileMenu al index con sus estilos y se agrega JS para visualizarlo con click en el icono de mobile.

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 /* navbar - elements */
 document.querySelector('.nav-email').addEventListener('click', openMenuDesktop);
+document.querySelector('.mobile-menu-icon').addEventListener('click', openMenuMobile);
 
 
 function openMenuDesktop(){
 
-    document.querySelector('.desktop-menu').classList.toggle('unable');
+    document.querySelector('.desktop-menu').classList.toggle('desktop-menu--unable');
+}
+function openMenuMobile(){
+
+    document.querySelector('.mobile-menu-container').classList.toggle('mobile-menu-container--unable');
 
 }
